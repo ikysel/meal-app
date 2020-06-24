85d82521bd300e4c0acda10aeb89d7fa798c24f6
Ivan Kyselov <kyselovivan@gmail.com>
2020-06-24 16:07:15 +0300

    added react navigation

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,21 @@
+import { createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer  } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+
+
+
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+
+const MealsNavigator = createStackNavigator({
+    Catigories: CategoriesScreen,
+    CategoryMeals: {
+        screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
+})
+
+export default createAppContainer(MealsNavigator);
