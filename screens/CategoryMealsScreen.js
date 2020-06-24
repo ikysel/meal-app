7df3735dbed145846ce291358a9176a8078da798
Ivan Kyselov <kyselovivan@gmail.com>
2020-06-24 12:13:34 +0300

    test

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <Viev>
+      <Text>Categories screen</Text>
+    </Viev>
+  );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoryMealsScreen;
