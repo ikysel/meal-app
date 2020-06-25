31a72942aad4927dc1ba624ad68bcc51443dfe6f
Ivan Kyselov <kyselovivan@gmail.com>
2020-06-25 17:17:53 +0300

    basic react-router added

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const CategoriesScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>Categories screen</Text>
+      <Button title="Go to GactegoryMeals" onPress={()=>{
+        props.navigation.navigate({ routeName: 'CategoryMeals'})
+      }}/>
     </View>
   );
 };
