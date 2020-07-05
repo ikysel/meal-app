4f86e5aaa6d276f8737da623aca2991b080678bf
Ivan Kyselov <kyselovivan@gmail.com>
2020-07-05 17:53:17 +0300

    heder navigation button "favorite" added

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import { MEALS } from '../data/dummy-data';
+import { MEALS } from "../data/dummy-data";
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -13,13 +15,18 @@ const MealDetailScreen = (props) => {
   );
 };
 
-MealDetailScreen.navigationOptions = navigationData => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal=> meal.id === mealId);
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title 
-  }
-}
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='favorite' iconName='ios-star' onPress={()=>console.log('Mark as favorite')}/>
+      </HeaderButtons>
+    ),
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
@@ -28,7 +35,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
-
 
 export default MealDetailScreen;
