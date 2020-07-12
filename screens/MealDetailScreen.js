64fc44d40528f128b85e03fb21c0f0dad9d8b552
Ivan Kyselov <kyselovivan@gmail.com>
2020-07-12 13:49:05 +0300

    display info on MealDetailScreen done

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,17 +1,47 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/dummy-data";
-import HeaderButton from '../components/HeaderButton';
+import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
+import Colors from "../constans/Colors";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  const ListItem = (props) => {
+    return (
+      <View style={styles.listItem}>
+        <Text>{props.children}</Text>
+      </View>
+    );
+  };
 
   return (
-    <View style={styles.screen}>
-      <Text>Categories screen</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+        <View style={styles.details}>
+          <DefaultText>{selectedMeal.duration}</DefaultText>
+          <DefaultText>{selectedMeal.complexity}</DefaultText>
+          <DefaultText>{selectedMeal.affordability}</DefaultText>
+        </View>
+        <Text style={styles.title}>Ingredients</Text>
+        <View>
+          {selectedMeal.ingredients.map((ingredient) => {
+            return <ListItem>{ingredient}</ListItem>;
+          })}
+        </View>
+        <Text style={styles.title}>Steps</Text>
+        <View>
+          {selectedMeal.steps.map((step) => {
+            return <ListItem>{step}</ListItem>;
+          })}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -22,17 +52,39 @@ MealDetailScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedMeal.title,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title='favorite' iconName='ios-star' onPress={()=>console.log('Mark as favorite')}/>
+        <Item
+          title="favorite"
+          iconName="ios-star"
+          onPress={() => console.log("Mark as favorite")}
+        />
       </HeaderButtons>
     ),
   };
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 15,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  listItem: {
+    borderColor: "#ccc",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginHorizontal: 10,
+    marginVertical: 5,
   },
 });
 
